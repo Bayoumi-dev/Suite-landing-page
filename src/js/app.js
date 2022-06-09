1608acbc86892a90fa4362b7e184518e13290f20
Ahmed Bayoumi <bayoumi.dev@gmail.com>
2022-06-09 21:11:22 +0200

    animations

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,31 @@
-const app = () => {}
+// Global Variable
+const sections = document.body.querySelectorAll("section")
+const loadingIntro = document.body.querySelector('.loading-intro');
+
+const app = () => {
+    // Loading Intro
+    setTimeout(() => {
+        loadingIntro.style.display = "none"
+        document.body.classList.add('content');
+    }, 2000);
+
+    // Add the "anim" class to the section when the section is on the viewport 
+    const options = {
+            threshold: 0.3,
+        },
+        observer = new IntersectionObserver((sections) => {
+            sections.forEach((sec) => {
+                sec.isIntersecting && sec.target.classList.add("anim");
+            });
+        }, options),
+        // Setup function to execute the observer
+        callObserver = (sec) => {
+            observer.observe(sec);
+        };
+    sections.forEach((sec) => {
+        sec.style.opacity = "0";
+        callObserver(sec);
+    });
+}
 
 export default app;
